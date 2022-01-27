4e66fd5bc91545335c26313d1022b734923d2f03
kevinspasiuk <kevin_spasiuk@hotmail.com>
2022-01-26 22:22:40 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,11 +1,212 @@
-## Ejercicio login formulario
+# Ejercicio Balanceo de Carga
 
 Este repositorio usa express y persiste las sesiones de usuarios en mongo atlas.
 También permite el login mediante Facebook.
 
-### Run
+## Run
 
-$  node index.js -p {PORT_NUMBER}
+$  node index.js -p {PORT_NUMBER} [-m cluster]
 
 
 Para selección del puerto mediante arg: -p {PORT_NUMBER}
+Para correr el server en modo cluster se debe agregar el argumento: -m cluster
+
+## Output del las diferentes invocaciones
+
+
+### NODEMON
+
+
+$  nodemon index.js -p 8081 -m cluster
+
+```
+[nodemon] 2.0.12
+[nodemon] to restart at any time, enter `rs`
+[nodemon] watching path(s): *.*
+[nodemon] watching extensions: js,mjs,json
+[nodemon] starting `node index.js -p 8081 -m cluster`
+8
+PID MASTER 50476
+[PID: 50500] Servidor express escuchando en el puerto 8081
+[PID: 50488] Servidor express escuchando en el puerto 8081
+[PID: 50487] Servidor express escuchando en el puerto 8081
+[PID: 50494] Servidor express escuchando en el puerto 8081
+[PID: 50507] Servidor express escuchando en el puerto 8081
+[PID: 50528] Servidor express escuchando en el puerto 8081
+[PID: 50514] Servidor express escuchando en el puerto 8081
+[PID: 50517] Servidor express escuchando en el puerto 8081
+```
+
+$ ps -e|grep node
+```
+50456 pts/0    00:00:00 node
+50476 pts/0    00:00:00 node
+50487 pts/0    00:00:00 node
+50488 pts/0    00:00:00 node
+50494 pts/0    00:00:00 node
+50500 pts/0    00:00:00 node
+50507 pts/0    00:00:00 node
+50514 pts/0    00:00:00 node
+50517 pts/0    00:00:00 node
+50528 pts/0    00:00:00 node
+```
+
+$ node index.js -p 8081 -m cluster
+
+```
+PID MASTER 50889
+[PID: 50902] Servidor express escuchando en el puerto 8081
+[PID: 50903] Servidor express escuchando en el puerto 8081
+[PID: 50910] Servidor express escuchando en el puerto 8081
+[PID: 50896] Servidor express escuchando en el puerto 8081
+[PID: 50931] Servidor express escuchando en el puerto 8081
+[PID: 50924] Servidor express escuchando en el puerto 8081
+[PID: 50943] Servidor express escuchando en el puerto 8081
+[PID: 50916] Servidor express escuchando en el puerto 8081
+```
+
+$ ps -e|grep node
+
+```
+50889 pts/0    00:00:00 node
+50896 pts/0    00:00:00 node
+50902 pts/0    00:00:00 node
+50903 pts/0    00:00:00 node
+50910 pts/0    00:00:00 node
+50916 pts/0    00:00:00 node
+50924 pts/0    00:00:00 node
+50931 pts/0    00:00:00 node
+50943 pts/0    00:00:00 node
+```
+
+### FOREVER
+
+$ forever start index.js -p 8081 -m cluster
+
+$ forever list
+
+```
+info:    Forever processes running
+data:        uid  command                                          script                      forever pid   id logfile                       uptime       
+data:    [0] Sv6m /home/kevin/.nvm/versions/node/v14.17.6/bin/node index.js -p 8081 -m cluster 51369   51376    /home/kevin/.forever/Sv6m.log 0:0:0:21.265 
+```
+
+$ ps -e|grep node
+```
+  51369 ?        00:00:00 node
+  51376 ?        00:00:00 node
+  51383 ?        00:00:00 node
+  51389 ?        00:00:00 node
+  51390 ?        00:00:00 node
+  51397 ?        00:00:00 node
+  51403 ?        00:00:00 node
+  51410 ?        00:00:00 node
+  51418 ?        00:00:00 node
+  51425 ?        00:00:00 node
+```
+
+### PM2
+
+**Modo fork:**
+
+$ pm2 start index.js
+
+$  pm2 list
+
+![image info](./images/pm2_fork.png)
+
+**Modo cluster:**
+
+$ pm2 start index.js --watch -i max
+
+![image info](./images/pm2_cluster.png)
+
+## NGINX
+
+
+Configuración NGINX:
+
+**Config 1, ambos server en modo fork:**
+
+
+```
+$ pm2 start index.js -- --p=8080 
+$ pm2 start index.js -- --p=8081 
+```
+
+Se redirige las consultas a /api/randoms al puerto 8081, el resto al puerto 8080
+
+```
+upstream node_app {
+		server 127.0.0.1:8080;
+}
+
+upstream node_app_api_randoms {
+		server 127.0.0.1:8081;
+}
+
+server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+
+
+	root /var/www/nginx;
+
+	server_name _;
+
+	location /api/randoms {
+		proxy_pass http://node_app_api_randoms;
+	}
+
+	location / {
+		proxy_pass http://node_app;
+	}
+
+  }
+}
+
+```
+
+**Config 2, redirección /api/randoms en cluster gestionado por NGINX:**
+
+```
+$ pm2 start index.js -- --p=8080 
+$ pm2 start index.js -- --p=8081 
+$ pm2 start index.js -- --p=8082 
+$ pm2 start index.js -- --p=8083
+$ pm2 start index.js -- --p=8084
+```
+
+```
+upstream node_app {
+		server 127.0.0.1:8080;
+}
+
+upstream node_app_api_randoms {
+    server 127.0.0.1:8081;
+    server 127.0.0.1:8082;
+    server 127.0.0.1:8083;
+    server 127.0.0.1:8084;
+}
+
+server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+
+
+	root /var/www/nginx;
+
+	server_name _;
+
+	location /api/randoms {
+		proxy_pass http://node_app_api_randoms;
+	}
+
+	location / {
+		proxy_pass http://node_app;
+	}
+
+  }
+}
+
+```
--- index.js
+++ index.js
@@ -6,124 +6,150 @@ const parseArgs = require('minimist');
 const { Strategy: FacebookStrategy } = require('passport-facebook');
 const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true};
 
-const args = parseArgs(process.argv.slice(2), {alias: {p: 'PUERTO'} });
+const args = parseArgs(process.argv.slice(2), {alias: {p: 'PUERTO', m: "MODO"} });
 const PORT = args.PUERTO || 8080
+const MODO = args.MODO || 'fork'
 const app = express();
 
-app.use(session({
-    store: MongoStore.create({
-      mongoUrl: 'mongodb+srv://admin:admin@clusterkevin.uq0gf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-      mongoOptions: advancedOptions    
-    }),
-    secret: `${process.env.SESSION_SECRET}`,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 60000
-    }
-}));
+const cluster = require('cluster')
+const numCPUs = require('os').cpus().length
 
-require('dotenv').config();
-const FACEBOOK_CLIENT_ID = process.env.FB_CLIENT_ID ;
-const FACEBOOK_CLIENT_SECRET = process.env.FB_CLIENT_SECRET;
+/* --------------------------------------------------------------------------- */
+/* MASTER */
+if (cluster.isMaster && MODO == 'cluster') {
+  console.log(numCPUs)
+  console.log(`PID MASTER ${process.pid}`)
 
-
-
-passport.use(new FacebookStrategy({
-    clientID: FACEBOOK_CLIENT_ID,
-    clientSecret: FACEBOOK_CLIENT_SECRET,
-    callbackURL: `http://localhost:${PORT}/auth/facebook/callback`,
-    profileFields: [ 'id', 'displayName', 'photos', 'emails' ],
-    scope: [ 'email' ]
-}, (accessToken, refreshToken, userProfile, done) => {
-    return done(null, userProfile);
-}));
-
-passport.serializeUser((user, cb) => {
-    cb(null, user);
-});
-
-passport.deserializeUser((obj, cb) => {
-    cb(null, obj);
-});
-
-app.use(express.json());
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-const exphbs = require('express-handlebars');
-
-app.engine('hbs', exphbs.engine({
-  extname: 'hbs',
-  defaultLayout: 'index.hbs'
-}));
-
-// app.set('view engine', 'handlebars')
-app.use(express.static('public'));
-app.set('views', './views');
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', function (req, res){
-  if (req.isAuthenticated()) {
-    res.render('loggeado.hbs', {nombre: req.user.displayName, 
-                                foto: req.user.photos[ 0 ].value,
-                                email: req.user.emails[ 0 ].value})
-  } else {
-    res.render('formulario.hbs')
+  for (let i = 0; i < numCPUs; i++) {
+      cluster.fork()
   }
-});
 
-/* Old form login*/
-app.post('/login', (req, res) => {
+  cluster.on('exit', worker => {
+      console.log('Worker', worker.process.pid, 'died', new Date().toLocaleString())
+      cluster.fork()
+  })
+}
+else {
 
-    console.log("Login! ", req.body.email )
-    const username = req.body.email
-    const password = req.body.password
+  /* --------------------------------------------------------------------------- */
+  /* WORKERS */
+  
+  app.use(session({
+      store: MongoStore.create({
+        mongoUrl: 'mongodb+srv://admin:admin@clusterkevin.uq0gf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+        mongoOptions: advancedOptions    
+      }),
+      secret: `${process.env.SESSION_SECRET}`,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 60000
+      }
+  }));
 
-    req.session.username = username
-    res.redirect("/")
-})
+  require('dotenv').config();
+  const FACEBOOK_CLIENT_ID = process.env.FB_CLIENT_ID ;
+  const FACEBOOK_CLIENT_SECRET = process.env.FB_CLIENT_SECRET;
 
 
-app.get('/auth/facebook', passport.authenticate('facebook'));
 
-app.get('/auth/facebook/callback', passport.authenticate('facebook', {
-  successRedirect: '/',
-  failureRedirect: '/faillogin'
-}));
+  passport.use(new FacebookStrategy({
+      clientID: FACEBOOK_CLIENT_ID,
+      clientSecret: FACEBOOK_CLIENT_SECRET,
+      callbackURL: `http://localhost:${PORT}/auth/facebook/callback`,
+      profileFields: [ 'id', 'displayName', 'photos', 'emails' ],
+      scope: [ 'email' ]
+  }, (accessToken, refreshToken, userProfile, done) => {
+      return done(null, userProfile);
+  }));
 
-app.get('/faillogin', (req, res) => {
-  res.render('login-error', {});
-})
+  passport.serializeUser((user, cb) => {
+      cb(null, user);
+  });
 
-app.post('/logout', (req, res) => {
-  const nombre = req.session.username
-  req.logout();
-  req.session.destroy(err => {
-        if (err) {
-          res.json({ status: 'Logout ERROR', body: err })
-        } else {
-          res.render('logout.hbs',{layout :"logout.hbs", nombre: nombre})
-        }
-      })
-})
+  passport.deserializeUser((obj, cb) => {
+      cb(null, obj);
+  });
 
-app.get('/info', (req, res) => {
-  res.send({info: {
-    argumentos: process.argv,
-    os: process.platform,
-    node_ver: process.version,
-    uso_memoria: process.memoryUsage(), 
-    path_ejecucion: process.cwd(),
-    process_id: process.pid,
-    project_folder: process.title
-  }});
-})
+  app.use(express.json());
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-const randomRouter = require('./routes/random');
-app.use('/api/randoms',randomRouter)
 
-app.listen(PORT, () => {
-  console.log(`Servidor express escuchando en el puerto ${PORT}`)
-})
+  const exphbs = require('express-handlebars');
+
+  app.engine('hbs', exphbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'index.hbs'
+  }));
+
+  // app.set('view engine', 'handlebars')
+  app.use(express.static('public'));
+  app.set('views', './views');
+  app.use(express.urlencoded({ extended: true }));
+
+  app.get('/', function (req, res){
+    if (req.isAuthenticated()) {
+      res.render('loggeado.hbs', {nombre: req.user.displayName, 
+                                  foto: req.user.photos[ 0 ].value,
+                                  email: req.user.emails[ 0 ].value})
+    } else {
+      res.render('formulario.hbs')
+    }
+  });
+
+  /* Old form login*/
+  app.post('/login', (req, res) => {
+
+      console.log("Login! ", req.body.email )
+      const username = req.body.email
+      const password = req.body.password
+
+      req.session.username = username
+      res.redirect("/")
+  })
+
+
+  app.get('/auth/facebook', passport.authenticate('facebook'));
+
+  app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+    successRedirect: '/',
+    failureRedirect: '/faillogin'
+  }));
+
+  app.get('/faillogin', (req, res) => {
+    res.render('login-error', {});
+  })
+
+  app.post('/logout', (req, res) => {
+    const nombre = req.session.username
+    req.logout();
+    req.session.destroy(err => {
+          if (err) {
+            res.json({ status: 'Logout ERROR', body: err })
+          } else {
+            res.render('logout.hbs',{layout :"logout.hbs", nombre: nombre})
+          }
+        })
+  })
+
+  app.get('/info', (req, res) => {
+    res.send({info: {
+      argumentos: process.argv,
+      os: process.platform,
+      node_ver: process.version,
+      uso_memoria: process.memoryUsage(), 
+      path_ejecucion: process.cwd(),
+      process_id: process.pid,
+      project_folder: process.title,
+      numcpu: numCPUs
+    }});
+  })
+
+  const randomRouter = require('./routes/random');
+  app.use('/api/randoms',randomRouter)
+
+  app.listen(PORT, () => {
+    console.log(`[PID: ${process.pid}] Servidor express escuchando en el puerto ${PORT}`)
+  })
+}
